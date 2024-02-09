ce5c89bf5b6f6941c866ac6860e4e81bae834fb2
Xelauk Dain <xelaukdain@gmail.com>
2024-02-08 19:57:51 -0700

    Updated Crime.js and added a new alert feature

--- assets/js/crime.js
+++ assets/js/crime.js
@@ -18,12 +18,21 @@ function getCrimeHistory(street) {
     .then(response => response.json())
     .then(data => {
       const totalCrimes = data.result.total;
+      const proBtn = document.getElementById('proceed-btn');
       const crimeP = document.createElement('p');
-      crimeP.textContent = 'Total results found: ' + totalCrimes;
+      const crimeAlert = document.getElementById('crime-alert');
+    if (totalCrimes > 500) {
+        crimeAlert.style.display = "block";
+        crimeAlert.querySelector('h3').textContent = 'Crime Alert';
+        crimeAlert.querySelector('p').textContent = 'Due to an unusually high crime rate this is a potenially dangerous area. Reported Incidents: ' + totalCrimes;
       document.body.appendChild(crimeP);
-    })
+      proBtn.addEventListener('click', function() {
+        crimeAlert.style.display = "none";
+      });
+    }
+})
 
-    // Creates an alert with the total number of crimes on the given street (for testing purposes)
+   //Creates an alert with the total number of crimes on the given street (for testing purposes)
     // .then(response => response.json())
     // .then(data => {
     //   alert('Total results found: ' + data.result.total);
@@ -35,6 +44,64 @@ function getCrimeHistory(street) {
     });
 
     // Logs the crime api data in the console
-    console.log(data);
+    JSON.stringify(data.result.total);
+    console.log(data.result.total);
   
 }
+
+// getCrimeHistory("N 7th St"); (for testing purposes)
+
+
+
+
+// <!-- Crime thingy -->
+// <div id="crime-alert" class="relative z-10" aria-labelledby="modal-title" role="dialog" aria-modal="true" style="display: none;">
+//     <!--
+//       Background backdrop, show/hide based on modal state.
+  
+//       Entering: "ease-out duration-300"
+//         From: "opacity-0"
+//         To: "opacity-100"
+//       Leaving: "ease-in duration-200"
+//         From: "opacity-100"
+//         To: "opacity-0"
+//     -->
+//     <div class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"></div>
+  
+//     <div class="fixed inset-0 z-10 w-screen overflow-y-auto">
+//       <div class="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+//         <!--
+//           Modal panel, show/hide based on modal state.
+  
+//           Entering: "ease-out duration-300"
+//             From: "opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+//             To: "opacity-100 translate-y-0 sm:scale-100"
+//           Leaving: "ease-in duration-200"
+//             From: "opacity-100 translate-y-0 sm:scale-100"
+//             To: "opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+//         -->
+//         <div class="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
+//           <div class="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
+//             <div class="sm:flex sm:items-start">
+//               <div class="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
+//                 <svg class="h-6 w-6 text-red-600" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+//                   <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126zM12 15.75h.007v.008H12v-.008z" />
+//                 </svg>
+//               </div>
+//               <div class="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
+//                 <h3 class="text-base font-semibold leading-6 text-gray-900" id="modal-title">Deactivate account</h3>
+//                 <div class="mt-2">
+//                   <p class="text-sm text-gray-500">Are you sure you want to deactivate your account? All of your data will be permanently removed. This action cannot be undone.</p>
+//                 </div>
+//               </div>
+//             </div>
+//           </div>
+//           <div class="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
+//             <button id="proceed-btn" type="button" class="inline-flex w-full justify-center rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 sm:ml-3 sm:w-auto">Proceed</button>
+//             <button type="button" class="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto">Go Back</button>
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+// </div>
+// <!--Footer credits student names, date of completion-->
