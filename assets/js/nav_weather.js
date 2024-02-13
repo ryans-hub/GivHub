ac08f826b90c4faaec71dba3562e5b071592fa39
Noah Sehman <noah.sehman@gmail.com>
2024-02-13 16:11:57 -0700

    Weather icons n stuff. you know what it izzz (cool face emoji)

--- assets/js/nav_weather.js
+++ assets/js/nav_weather.js
@@ -17,13 +17,56 @@ async function homeWeather (p) {
 		rain: pizza.day.totalprecip_in,
 	};
     const atag = document.getElementById("weatha");
-	basicHeader = document.createElement("h1");
-	basicHeader.textContent = "Current Weather\nPhoenix, AZ";
-	atag.appendChild(basicHeader);
-	const basCont = document.createElement("p");
-	atag.appendChild(basCont);
+	const sects = atag.children;
+	console.logshaot;
+
+	const temptation = sects[0];
+	if (basic.temp >= 85) {
+		temptation.children[0].style.display = "inline-block";
+		temptation.children[0].style.float = "left";
+	}
+	else {
+		temptation.children[1].style.display = "inline-block";
+		temptation.children[1].style.float = "left";
+	}
+	let wordy = "It's currently " + basic.temp + " degrees";
+	let wordly = "And it feels like " + basic.feelsLike + " degrees";
+	temptation.children[2].textContent = wordy;
+	temptation.children[3].textContent = wordly;
+
+	const condy = sects[1];
+	let shaot = basic.condt.toLowerCase();
+	if (shaot == "sunny") {
+		condy.children[0].style.display = "inline-block";
+		condy.children[0].style.float = "left";
+		var condyT = "It's Sunny!! Wahooo";
+		var funt = "Go outside. Have fun!!";
+	}
+	else if (shaot.includes("rain") || shaot.includes("snow") || shaot.includes("sleet") || shaot.includes("ice")) {
+		condy.children[2].style.display = "inline-block";
+		condy.children[2].style.float = "left";
+		var condyT = "Woah there partner it's kinda stormy!";
+		var funt = "Why are you even considering?";
+	}
+	else {
+		condy.children[1].style.display = "inline-block";
+		condy.children[1].style.float = "left";
+		var condyT = "There are clouds yay!!";
+		var funt = "Tread lightly. Don't die please";
+	}
+	condy.children[3].textContent = condyT;
+	condy.children[4].textContent = funt;
+
+	const sunst = sects[2];
+	sunst.children[0].style.float = "left";
+	const pp = sunst.children[1];
+	const ppp = sunst.children[2];
+	let ppt = "The sun will rise at " + basic.sunRS[0];
+	pp.textContent = ppt;
+	let pppt = "The sun will set at " + basic.sunRS[1];
+	ppp.textContent = pppt;
+
 	bqc = "Currently " + basic.temp + " degrees and " + basic.condt +  "\nFeels Like: " + basic.feelsLike + "\nThe sun will rise at " + basic.sunRS[0] + " and will set at " + basic.sunRS[1]  + "\nIt is set to rain about " + basic.rain + " inches today";
-	basCont.textContent = bqc;
 }
 
 //---------------------------------------------------------------------------//
